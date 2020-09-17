91d26bd97a6656cf87cbb566f51e28de0eb2b639
lidemin <1425889927@qq.com>
2020-09-17 17:58:16 +0800

    layout

--- src/app.config.ts
+++ src/app.config.ts
@@ -6,4 +6,25 @@ export default {
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black',
   },
+  tabBar: {
+    color: '#000',
+    borderStyle: 'white',
+    selectedColor: '#fff',
+    backgroundColor: '#185000',
+    list: [
+      {
+        pagePath: 'pages/home/index',
+        text: '首页',
+        iconPath: '',
+        selectedIconPath: '',
+      },
+      {
+        pagePath: 'pages/login/index',
+        text: '登录',
+        iconPath: '',
+        selectedIconPath: '',
+      },
+    ],
+  },
+  usingComponents: {},
 }
